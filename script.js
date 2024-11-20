aca919f64970c643357ba1c8e9ab0792a202efd2
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-20 15:55:32 +0300

    Add resizeGrid()

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const container = document.querySelector('.container');
 const gridButton = document.querySelector('#gridButton');
 
+// Create 16x16 grid
 for (let i = 0; i < 256; i ++) {
   const div = document.createElement('div');
   div.classList.add('block');
   container.appendChild(div);
+}
+
+gridButton.addEventListener('click', resizeGrid);
+
+// Creates grid with size of prompt x prompt 
+function resizeGrid() {
+  const sideSize = prompt('Squares per side:');
+  const gridSize = sideSize * sideSize;
+  container.innerHTML = '';
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('block');
+    div.style.height = `${960 / sideSize}px`;
+    div.style.width = `${960 / sideSize}px`;
+    container.appendChild(div);
+  }
 }
