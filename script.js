750fb21dae6c16957772ffd4958f3e2a31054752
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-20 15:27:07 +0300

    Add class to div
    Its needed because container div would take all the parameters
    also, what is not needed. So i separated those that are
    needed to be highlighted

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
+const gridButton = document.querySelector('#gridButton');
 
 for (let i = 0; i < 256; i ++) {
   const div = document.createElement('div');
+  div.classList.add('block');
   container.appendChild(div);
 }
